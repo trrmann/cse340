80130aa73f19fba72d0827829e5bcd34f6cd67c7
trrmann <113477307+trrmann@users.noreply.github.com>
2026-01-21 15:20:25 -0500

    cleaned up from git issues on non-work box

--- controllers/baseController-Mann.js
+++
@@ -1,9 +0,0 @@
-const utilities = require('../utilities/');
-const baseController = {};
-
-baseController.buildHome = async function (req, res) {
-  const nav = await utilities.getNav();
-  res.render('index', { title: 'Home', nav });
-};
-
-module.exports = baseController;
