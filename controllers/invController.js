ca605a63a0e71f45be14472e8251b5480c81fd61
trrmann <trmann70@hotmail.com>
2026-01-22 17:13:40 -0500

    added error trap and debugging

--- controllers/invController.js
+++ controllers/invController.js
@@ -7,16 +7,21 @@ const invCont = {};
  * Build inventory by classification view
  * ********************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classification_id;
-  const data = await invModel.getInventoryByClassificationId(classification_id);
-  const grid = await utilities.buildClassificationGrid(data);
-  let nav = await utilities.getNav();
-  const className = data[0].classification_name;
-  res.render('./inventory/classification', {
-    title: className + ' vehicles',
-    nav,
-    grid,
-  });
+  try {
+    const classification_id = req.params.classification_id;
+    const data =
+      await invModel.getInventoryByClassificationId(classification_id);
+    const grid = await utilities.buildClassificationGrid(data);
+    let nav = await utilities.getNav();
+    const className = data[0].classification_name;
+    res.render('./inventory/classification', {
+      title: className + ' vehicles',
+      nav,
+      grid,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = invCont;
--- server.js
+++ server.js
@@ -33,6 +33,24 @@ app.use('/inv', inventoryRoute);
 app.get('/', baseController.buildHome);
 
 /* ***********************
+ * Error Handling Middleware
+ *************************/
+// 404 handler
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .send(
+      `<h1>404 - Not Found</h1><p>The page you are looking for does not exist.</p><p>Requested: ${req.url}</p>`
+    );
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error('Error:', err);
+  res.status(500).send(`<h1>500 - Server Error</h1><p>${err.message}</p>`);
+});
+
+/* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
