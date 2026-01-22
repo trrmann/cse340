7597a7e85573761891be06f9a55c48a212cd7e49
trrmann <trmann70@hotmail.com>
2026-01-22 15:00:38 -0500

    fix the url for remote workflow

--- scripts/Push.ps1
+++ scripts/Push.ps1
@@ -8,7 +8,7 @@ if ($exitCode -eq 0) {
     Write-Host "Push successful."
     # Open Render deploy page and end site in default browser
     $renderDeployUrl = "https://dashboard.render.com/static/srv-d5ff83ur433s73av55p0"
-    $renderSiteUrl = "https://sleep-outside-team-04-repository.onrender.com/"
+    $renderSiteUrl = "https://cse340tm.onrender.com/"
     Start-Process $renderDeployUrl
     Start-Process $renderSiteUrl
 } else {
